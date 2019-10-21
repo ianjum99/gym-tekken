39fe7aec8992f3e5249508edea8444ec020fa81a
ianjum99 <44471479+ianjum99@users.noreply.github.com>
2019-10-21 18:04:56 +0000

    Update tekken_env.py

--- gym_tekken/envs/tekken_env.py
+++ gym_tekken/envs/tekken_env.py
@@ -1,1 +1,17 @@
+import gym
+from gym omport error, spaces, utils
+from gym.utils import seeding
 
+class tekkenEnv(gym.env):
+  metadata = {'render.modes': ['humsn']}
+  def __init__self:
+    pass
+
+  def step(self, action):
+    pass
+ 
+  def reset(self):
+    pass
+  
+  def render(self, mode='human', close =False):
+    pass
