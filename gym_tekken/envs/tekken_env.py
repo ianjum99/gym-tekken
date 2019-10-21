b487f7ce8c74dff178dc2c4d45ee2d946a581567
ianjum99 <44471479+ianjum99@users.noreply.github.com>
2019-10-21 18:10:50 +0000

    Update tekken_env.py

--- gym_tekken/envs/tekken_env.py
+++ gym_tekken/envs/tekken_env.py
@@ -3,15 +3,19 @@ from gym import error, spaces, utils
 from gym.utils import seeding
 
 class tekkenEnv(gym.env):
-  metadata = {'render.modes': ['humsn']}
-  def __init__self:
-    pass
+  """A tekken 7 environment for OpenAI gym"""
+  metadata = {'render.modes': ['human']}
+  
+  def __init__(self):
+    super(tekkenEnv, self).__init__()
+    #define action and observation space
+    #must be gym.spaces objects
 
   def step(self, action):
-    pass
+    #execute one step in the environment at a  time
  
   def reset(self):
-    pass
+    #reset environment to an initial state
   
   def render(self, mode='human', close =False):
-    pass
+    #render the environment to a screen
