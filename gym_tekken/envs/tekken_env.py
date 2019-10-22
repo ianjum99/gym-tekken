3037c06e52cf75971c1e7ad07ff3cc0c3c45daaa
ianjum99 <44471479+ianjum99@users.noreply.github.com>
2019-10-22 18:34:05 +0000

    Update tekken_env.py

--- gym_tekken/envs/tekken_env.py
+++ gym_tekken/envs/tekken_env.py
@@ -1,6 +1,21 @@
 import gym
 from gym import error, spaces, utils
 from gym.utils import seeding
+import numpy as np
+from PIL import ImageGrab
+imprt cv2
+import time
+
+LastTime=time.time()
+while(True):
+  scrn= np.array(ImageGrab.grab(bbox=(0,40, 800, 640)))
+  
+  print('loop took {} seconds'.format(time.time()-LastTime))
+  LastTime=time.time()
+  cv2.imshow('window', cv2.cvtColor(scrn , cv2.COLOR_BGR2RGB)
+  if cv2.waitKey(25) & 0xFF == ord('q'):
+    cv2.destroyAllWindows()
+    break
 
 class tekkenEnv(gym.env):
   """A tekken 7 environment for OpenAI gym"""
