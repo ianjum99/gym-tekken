a592bf1469fc33f71bb10171dced6bbcc5b2c216
ianjum99 <44471479+ianjum99@users.noreply.github.com>
2019-10-23 16:04:49 +0000

    Update tekken_env.py

--- gym_tekken/envs/tekken_env.py
+++ gym_tekken/envs/tekken_env.py
@@ -5,17 +5,33 @@ import numpy as np
 from PIL import ImageGrab
 imprt cv2
 import time
+import pyautogui
 
+"""
+for i in list(range(4))[::-1]:
+  print(i+1)
+  time.sleep(1)
+  
+  
+
+def processImg(orgnImg):
+  processedImg=cv2.cvtColor(orgnImg, cv2.COLOR_BGR2GRAY)#converting to gray as this simplifies values of pixels and can be processed by a CNR
+  processedImg=cv2.Canny(processedImg, threshold1=200, threshold2=300)
+  return processedImg
+  
 LastTime=time.time()
 while(True):
   scrn= np.array(ImageGrab.grab(bbox=(0,40, 800, 640)))
+  newScrn=processImg(scrn)
   
   print('loop took {} seconds'.format(time.time()-LastTime))
   LastTime=time.time()
-  cv2.imshow('window', cv2.cvtColor(scrn , cv2.COLOR_BGR2RGB)
+  cv2.imshow('window', newScrn)
+  #cv2.imshow('window2', cv2.cvtColor(scrn , cv2.COLOR_BGR2RGB)
   if cv2.waitKey(25) & 0xFF == ord('q'):
     cv2.destroyAllWindows()
     break
+"""
 
 class tekkenEnv(gym.env):
   """A tekken 7 environment for OpenAI gym"""
